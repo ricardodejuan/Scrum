98438d02b5c4e8c4a810e34d100df2d86aea8361
ricardodejuan <raicar@gmail.com>
2014-12-31 12:20:44 +0100

    Changed permissions in certificates.

--- pfc_scrum/scripts/generate-ssl-certs.sh
+++ pfc_scrum/scripts/generate-ssl-certs.sh
@@ -14,4 +14,4 @@ openssl genrsa -out ./config/sslcerts/key.pem -aes256 1024
 openssl req -new -key ./config/sslcerts/key.pem -out ./config/sslcerts/csr.pem
 openssl x509 -req -days 9999 -in ./config/sslcerts/csr.pem -signkey ./config/sslcerts/key.pem -out ./config/sslcerts/cert.pem
 rm ./config/sslcerts/csr.pem
-chmod 777 ./config/sslcerts/key.pem ./config/sslcerts/cert.pem
+chmod 600 ./config/sslcerts/key.pem ./config/sslcerts/cert.pem
